18203c235f1474288bbe668f57ebfd80ab0111c6
het09 <hetgodhani7@gmail.com>
2020-05-22 20:27:50 +0530

    Learned Search element from linked list

---
+++ Linked_List/src/Search_Element.java
@@ -0,0 +1,53 @@
+
+class Node
+{
+	int data;
+	Node next;
+	Node(int d){
+		data = d;
+		next = null;
+	}
+}
+
+public class Search_Element {
+
+	Node head;
+	public void push(int new_data) {
+		
+		Node new_node = new Node(new_data);
+		
+		new_node.next = head;
+		
+		head = new_node;
+	}
+	
+	public boolean search(Node head, int x) {
+		
+		Node current = head;
+		while(current != null) {
+			if(current.data == x)
+				return true;
+			current = current.next;
+		}
+		return false;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Search_Element llist = new Search_Element();
+		
+		llist.push(10); 
+        llist.push(30); 
+        llist.push(11); 
+        llist.push(21); 
+        llist.push(14); 
+  
+        if (llist.search(llist.head, 15)) 
+            System.out.println("Yes"); 
+        else
+            System.out.println("No"); 
+
+	}
+
+}
